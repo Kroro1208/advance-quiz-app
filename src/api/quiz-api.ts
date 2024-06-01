fb55b3819a8485b57ac2fcdbf5338f6b5da9ee6b
kuroro1208 <yamagata_7580@yahoo.co.jp>
2024-06-01 13:42:45 +0900

    取得したjsonファイルを日本語で表示させた

--- src/api/quiz-api.ts
+++ src/api/quiz-api.ts
@@ -1,5 +1,6 @@
 import axios from "axios";
 import { FetchQuizCategoriesResponse, QuizCategory } from "../types/quiz-type";
+import { categoryTranslationMap } from "./translate";
 const BASE_URL = "http://opentdb.com";
 
 export class QuizAPI {
@@ -7,6 +8,9 @@ export class QuizAPI {
     const { data } = await axios.get<FetchQuizCategoriesResponse>(
       `${BASE_URL}/api_category.php`
     );
-    return data.trivia_categories;
+    return data.trivia_categories.map((category) => ({
+      ...category,
+      name: categoryTranslationMap[category.name] || category.name,
+    }));
   }
 }
---
+++ src/api/translate.ts
@@ -0,0 +1,29 @@
+export const categoryTranslationMap: { [key: string]: string } = {
+  "General Knowledge": "一般知識",
+  "Entertainment: Books": "エンターテイメント: 本",
+  "Entertainment: Film": "エンターテイメント: 映画",
+  "Entertainment: Music": "エンターテイメント: 音楽",
+  "Entertainment: Musicals & Theatres":
+    "エンターテイメント: ミュージカル & 劇場",
+  "Entertainment: Television": "エンターテイメント: テレビ",
+  "Entertainment: Video Games": "エンターテイメント: ビデオゲーム",
+  "Entertainment: Board Games": "エンターテイメント: ボードゲーム",
+  "Science & Nature": "科学 & 自然",
+  "Science: Computers": "科学: コンピュータ",
+  "Science: Mathematics": "科学: 数学",
+  "Mythology": "神話",
+  "Sports": "スポーツ",
+  "Geography": "地理",
+  "History": "歴史",
+  "Politics": "政治",
+  "Art": "アート",
+  "Celebrities": "有名人",
+  "Animals": "動物",
+  "Vehicles": "乗り物",
+  "Entertainment: Comics": "エンターテイメント: コミック",
+  "Science: Gadgets": "科学: ガジェット",
+  "Entertainment: Japanese Anime & Manga":
+    "エンターテイメント: 日本のアニメ & マンガ",
+  "Entertainment: Cartoon & Animations":
+    "エンターテイメント: カートゥーン & アニメーション",
+};
